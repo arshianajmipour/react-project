359f3f6015da2a82e601d8ac53ff35cd68e44c01
arshianajmipour <arshianajmipour@gmail.com>
2022-05-22 12:06:55 +0430

    routing problem

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ function App() {
   return (
 <div className="page">
      <Header/>
-     <BrowserRouter>
-         <Routes>
+     <BrowserRouter basename={process.env.PUBLIC_URL}>
+         <Routes >
              <Route exact path="/" element={<Home/>}/>
              <Route exact path="/Contact" element={<Contact/>}/>
              <Route exact path="/About" element={<About/>}/>
